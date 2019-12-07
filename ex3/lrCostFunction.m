ea7e3c52fbcf8036cdc61a35d18de5b802250728
hyanx <huiyana@foxmail.com>
2019-12-07 21:15:18 +0800

    lrCostFunction vectorizing

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -34,16 +34,22 @@ grad = zeros(size(theta));
 %           temp = theta; 
 %           temp(1) = 0;   % because we don't add anything for j = 0  
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
-%
 
 
+%%% This exercise same as ex2\constFunctionReg.m so I copy it quickly
 
+h = sigmoid(X * theta);
+grad_ = 1/m * X' *(h - y);
+theta(1) = 0;
+reg_grad_term = lambda / m * theta;
+grad = grad_ + reg_grad_term;
 
-
-
-
-
-
+first_term = -y' * log(h);
+second_term = (1 .- y)' * log(1 .- h);
+J_ = 1/m * (first_term - second_term);
+theta(1) = 0;
+regularization_term = lambda / (2 * m) * (theta' * theta);
+J = J_ + regularization_term;
 
 % =============================================================
 
